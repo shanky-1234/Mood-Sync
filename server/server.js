b7cc7671fb8208043920e26145cd6a10cda3f9dc
shanky-1234 <tuladharshashank2@gmail.com>
2026-04-06 21:42:35 +0545

    Server Fix

--- server/server.js
+++ server/server.js
@@ -16,7 +16,10 @@ const rewardRouter = require('./Routes/dailyRewardRoute')
 
 const app=express()
 
-app.use(cors())
+app.use(cors({
+    origin: ["http://localhost:5173", "https://mood-sync-014s.onrender.com"],
+    credentials: true
+}))
 app.use(express.json())
 const port = process.env.PORT || 3000
 
